4e9d65d34b36a9140c978e3ea0263b964c577904
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-23 17:18:46 +0300

    Create category and product entities

---
+++ src/main/resources/db/migration/V6__add_categories_and_products.sql
@@ -0,0 +1,19 @@
+-- create categories table
+CREATE TABLE categories
+(
+    id TINYINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- create products table
+CREATE TABLE products
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    category_id TINYINT,
+    CONSTRAINT  fk_category
+        FOREIGN KEY (category_id) REFERENCES categories (id)
+            ON DELETE  RESTRICT
+);
+
