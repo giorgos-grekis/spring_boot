92b80ff8ba7bdeb01a93951d5c92844e20f459e3
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-24 14:30:00 +0300

    clean unused code

--- src/main/java/org/cisu/store/StoreApplication.java
+++ src/main/java/org/cisu/store/StoreApplication.java
@@ -1,23 +1,21 @@
 package org.cisu.store;
 
-import org.cisu.store.entities.Address;
-import org.cisu.store.entities.Profile;
-import org.cisu.store.entities.Tag;
-import org.cisu.store.entities.User;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
 
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-        var user =  User.builder()
-                .name("John")
-                .email("john@gmail.com")
-                .password("password")
-                .build();
+//        var user =  User.builder()
+//                .name("John")
+//                .email("john@gmail.com")
+//                .password("password")
+//                .build();
 
 
 //        // add address
@@ -42,14 +40,14 @@ public class StoreApplication {
 
 //        // add profile
 
-        var profile = Profile.builder()
-                        .bio("bio")
-                        .build();
-
-        user.setProfile(profile);
-        profile.setUser(user);
-
-        System.out.println(user);
+//        var profile = Profile.builder()
+//                        .bio("bio")
+//                        .build();
+//
+//        user.setProfile(profile);
+//        profile.setUser(user);
+//
+//        System.out.println(user);
 
 
     }
