9093fd9cd55cae791c4e64a9f2f10d9bd07a2ce1
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-28 17:46:03 +0300

    query parameters

--- src/main/java/com/cisu/store/controllers/UserController.java
+++ src/main/java/com/cisu/store/controllers/UserController.java
@@ -4,13 +4,12 @@ import com.cisu.store.dtos.UserDto;
 import com.cisu.store.mappers.UserMapper;
 import com.cisu.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +20,17 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public List<UserDto> getAllUser() {
-        return userRepository.findAll()
+    public List<UserDto> getAllUser(
+           @RequestParam(
+                   name = "sort",
+                   required = false,
+                   defaultValue = "") String sortBy
+    ) {
+       if (!Set.of("name", "email").contains(sortBy))
+           sortBy = "name";
+
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
