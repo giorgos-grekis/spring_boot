af7eca68eeda876f55dbff5b2ee39dafaf1c95ff
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-28 15:49:15 +0300

    create dynamic routes

--- src/main/java/com/cisu/store/controllers/UserController.java
+++ src/main/java/com/cisu/store/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.cisu.store.entities.User;
 import com.cisu.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +12,18 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-
-    @RequestMapping("/users")
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUser() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
