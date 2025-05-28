6b67ff8e21eb42eb374051414957b2ffb6c8a246
giorgos-grekis <georgrekis.github@gmail.com>
2025-05-28 15:08:23 +0300

    passing data to html views

--- src/main/java/com/cisu/store/controllers/HomeController.java
+++ src/main/java/com/cisu/store/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.cisu.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // A bean is an object that manage by the spring framework
@@ -9,12 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "cisu");
+
+        return "index";
     }
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
+
 }
