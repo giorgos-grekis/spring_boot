1a6a2ebdafe315c693fe50c645cd704fc3debd71
giorgos-grekis <georgrekis.github@gmail.com>
2025-09-28 20:12:59 +0300

    improve: error handling if pass invalid data to body

--- src/main/java/com/cisu/store/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/cisu/store/controllers/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.cisu.store.controllers;
 
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,12 +17,52 @@ import java.util.UUID;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Handle invalid body
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUnreadableMessage(
+            HttpMessageNotReadableException ex) {
+        // Init  general message
+        String errorMessage = "Invalid request body format or type error.";
+
+        // We check if there is a 'cause' to get more details. Jackson (JsonMappingException)
+        Throwable rootCause = ex.getRootCause();
+
+        if (rootCause instanceof InvalidFormatException) {
+            // Handle error and display right message
+            InvalidFormatException ife = (InvalidFormatException) rootCause;
+
+            // get the failed field
+            String fieldName = ife.getPath().stream()
+                    .map(reference -> reference.getFieldName())
+                    .filter(java.util.Objects::nonNull)
+                    .findFirst()
+                    .orElse("N/A");
+
+            // create a more specific message
+            errorMessage = String.format(
+                    "Invalid value '%s' for field '%s'. Expected type: %s",
+                    ife.getValue(),
+                    fieldName,
+                    ife.getTargetType().getSimpleName()
+            );
+
+        } else if (rootCause != null) {
+            // If the root is something else (e.g., JsonParseException), we use its message.
+            errorMessage = "Malformed JSON: " + rootCause.getLocalizedMessage();
+        }
+
+
+        return ResponseEntity.badRequest().body(
+                Map.of("error", errorMessage)
+        );
+    }
+
     // Handle @Valid validation errors
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(
             MethodArgumentNotValidException exception
     ) {
-
         var errors = new HashMap<String, String>();
 
         exception.getBindingResult()
